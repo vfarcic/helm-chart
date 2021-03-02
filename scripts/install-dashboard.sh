386ecc952f588689f11538ebaae60f8b41f4f814
aleksej.paschenko <aleksej.paschenko@yandex.ru>
2021-03-02 19:44:26 +0300

    Fix dashboard upgrading (#128)

--- scripts/install-dashboard.sh
+++ scripts/install-dashboard.sh
@@ -7,41 +7,16 @@ if [ "x$DASHBOARD_ENABLED" != "xtrue" ]; then
   exit 0
 fi
 
-$SHIPA_CLIENT app-info -a dashboard > /dev/null 2>&1
-
-if [ $? = 1 ]; then
-  echo "Creating the dashboard app"
-  $SHIPA_CLIENT app-create dashboard static \
-      --framework=shipa-framework \
-      --team=shipa-admin-team \
-      -e SHIPA_ADMIN_USER=$USERNAME
-fi
+echo "Creating the dashboard app"
+$SHIPA_CLIENT app-create dashboard static \
+    --framework=shipa-framework \
+    --team=shipa-admin-team \
+    -e SHIPA_ADMIN_USER=$USERNAME
 
 NETDATA_CLIENT_CERT=$(kubectl get secret/shipa-certificates -o json | jq ".data[\"netdata-client.crt\"]" | base64 -d)
 NETDATA_CLIENT_KEY=$(kubectl get secret/shipa-certificates -o json | jq ".data[\"netdata-client.key\"]" | base64 -d)
 
 $SHIPA_CLIENT env-set -a dashboard NETDATA_CLIENT_KEY="$NETDATA_CLIENT_KEY" NETDATA_CLIENT_CERT="$NETDATA_CLIENT_CERT"
-
-EVENT_ID=$($SHIPA_CLIENT event-list --target=app --target-value=dashboard --kind=app.deploy | grep true | head -n 1 | cut -d "|" -f 2)
-
-if [ "x$EVENT_ID" != "x" ]; then
-  echo "Found app.deploy event for the dashboard: $EVENT_ID"
-  echo "Checking image used to deploy the dashboard"
-  SAME_IMAGE=$($SHIPA_CLIENT event-info $EVENT_ID | grep -e "$DASHBOARD_IMAGE$")
-
-  if [ "x$SAME_IMAGE" != "x" ]; then
-    echo "The dashboard uses the same image : $DASHBOARD_IMAGE"
-
-    # That's helm upgrade, we should wait for a new shipa api
-    sleep 140
-
-    #
-    # Let's restart the dashboard, there is a chance that a user has changed a license (Free -> Pro)
-    #
-    $SHIPA_CLIENT app-restart -a dashboard
-    exit 0
-  fi
-fi
 
 COUNTER=0
 until $SHIPA_CLIENT app-deploy -a dashboard -i $DASHBOARD_IMAGE
@@ -55,4 +30,3 @@ do
     fi
     
 done
-
